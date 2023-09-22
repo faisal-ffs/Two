68bfe991b0da398aade8cdab50c1fc099a92623d
faisal-ffs <faisalsabir.ffs@gmail.com>
2023-09-22 21:30:27 +0500

    form inputs

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Task from "./Components/Task/index"
+import Exp from "./Components/Exp/index"
+function App() {
+  return (
+    <>
+      <Task/>
+      <Exp/>
+    </>
+  )
+}
+
+export default App
