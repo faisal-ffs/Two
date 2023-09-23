c9537c839df3c91604394863b340b711550422c5
faisal-ffs <faisalsabir.ffs@gmail.com>
2023-09-23 17:56:58 +0500

    data

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import Expenseitem from './Components/Expenseitem'
-import NewExpense from "./Components/NewExpense"
+
 
 function App() {
 
@@ -17,7 +17,7 @@ function App() {
       <Expenseitem title={expenses[1].title} amount={expenses[1].amount} date={expenses[1].date}></Expenseitem>
       <Expenseitem title={expenses[2].title} amount={expenses[2].amount} date={expenses[2].date}></Expenseitem>
       <Expenseitem title={expenses[3].title} amount={expenses[3].amount} date={expenses[3].date}></Expenseitem>
-      <NewExpense/>
+     
       
     </div>
   )
