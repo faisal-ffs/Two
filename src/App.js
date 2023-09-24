c6971eedc286b8b63d35d38243875505935ddd1e
faisal-ffs <faisalsabir.ffs@gmail.com>
2023-09-24 14:06:57 +0500

    redering

--- src/App.js
+++ src/App.js
@@ -6,14 +6,14 @@ function App() {
 
   const expenses = [
    { id:'1' , title : ' Car  insurance',amount:"294.67",date:new Date (2021,2,28)},
-   {id:'2' ,title : ' Toilet paper',amount:"256.29",date:new Date (2023,2,8)},
-   { id:'3', title : ' News tv',amount:"203.56",date:new Date (2022,6,11)},
+   {id:'2' ,title : ' Toilet paper',amount:"256.29",date:new Date (2023,12,8)},
+   { id:'3', title : ' News tv',amount:"203.56",date:new Date (2022,12,11)},
    { id:'4',title : ' Extra',amount:"234.7",date:new Date (2019,4,8)},
   ];
 const addExpenseHandler = expense => {
   console.log('In App.js');
   console.log(expense);
-};
+};     
   return (
     <div>
     <NewExpense onAddExpense={addExpenseHandler}/>
--- src/Components/ExpenseForm.js
+++ src/Components/ExpenseForm.js
@@ -46,7 +46,7 @@ const ExpenseForm = (props) => {
         </div>
         <div className="new-expense__control">
           <label>Date</label>
-          <input type="date" min="2019-01-01" max="2022-03-06" value={enteredDate} onChange={dateChangeHandler} />
+          <input type="date" min="2019-01-01" max="2022-03-31" value={enteredDate} onChange={dateChangeHandler} />
         </div>
       </div>
       <div className="new-expense__actions">
